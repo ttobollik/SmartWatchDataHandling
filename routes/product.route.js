70e2ca3e7589257b7474d9fb4ce1161e03479653
Theresa Tobollik <theresa.joana.tobollik@uni-weimar.de>
2020-01-11 17:15:16 +0100

    change database name

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,13 +6,14 @@ const Joi = require('joi');
 //let username = process.env.username_mogodb;
 //let password = process.env.password_mongodb;
 const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb://localhost:27017"
+const dbName = process.env.MONGODB_URI ? "heroku_2wh2qzqj" : "test"
 
 //getting all objects in DB
 router.get('/', (req, res) => {
     const dbClient = new MongoClient(url);
     //connect() returns a promise. 2 Ways to have then. If i pass 1 function i need to pass catch as well. Or two functions, second for error
     dbClient.connect().then(() => {
-        dbClient.db("test").collection("products").find({}).toArray().then((products) => {
+        dbClient.db(dbName).collection("products").find({}).toArray().then((products) => {
             res.status(200).send(products)
         })
     }).catch((err) => {
@@ -31,7 +32,7 @@ router.post('/', (req, res) => {
             name: req.body.name,
             price: req.body.price
         }
-        dbClient.db("test").collection("products").insertOne(new_product).then((r) => {
+        dbClient.db(dbName).collection("products").insertOne(new_product).then((r) => {
             res.status(200).send("success ")
         })
     }).catch((err) => {
@@ -44,7 +45,7 @@ router.post('/', (req, res) => {
 router.put('/:name', (req, res) => {
     const dbClient = new MongoClient(url);
     dbClient.connect().then(() => {
-        dbClient.db("test").collection("products").updateMany({name: req.params.name}, {$set: {price: req.body.price}},
+        dbClient.db(dbName).collection("products").updateMany({name: req.params.name}, {$set: {price: req.body.price}},
             (err, result) => { 
                 if(err) {res.status(500).send(err)}
                 else if (result.modifiedCount === 0) {res.status(500).send("no objects modified. make sure required object exists in db.")}
@@ -58,7 +59,7 @@ router.put('/:name', (req, res) => {
 router.delete('/:name', (req, res) => {
     const dbClient = new MongoClient(url);
     dbClient.connect().then(() => {
-        dbClient.db("test").collection("products").findOneAndDelete({name: req.params.name}).then((doc,err) =>{
+        dbClient.db(dbName).collection("products").findOneAndDelete({name: req.params.name}).then((doc,err) =>{
             if(err) {res.status(500).send(`no ${req.params.name} in list`) + err}
             else {res.status(200).send(`sucessfull deletion of ${req.params.name}`)}
         }).catch((err)=> {res.status(400).send(`could not delete ${req.params.name}` + err)})
